d972316d79d1c0bb41b64c2a2bd8aeb75b64bd58
michal <polak_m@o2.pl>
2018-09-13 18:08:37 +0200

    remove buy and sell licences add remove default minters

--- contracts/MichalPolakToken.sol
+++ contracts/MichalPolakToken.sol
@@ -10,16 +10,14 @@ import "openzeppelin-solidity/contracts/token/ERC20/ERC20Burnable.sol";
 
 contract MichalPolakToken is ERC20, ERC20Detailed, ERC20Capped, ERC20Burnable, Ownable {
   
-    string public constant TOKEN_NAME = "Michal Polak Token";
-    string public constant TOKEN_SYMBOL = "MPT";
-    uint8 public constant TOKEN_DECIMALS = 18;
+    string  constant TOKEN_NAME = "Michal Polak Token";
+    string  constant TOKEN_SYMBOL = "MPT";
+    uint8  constant TOKEN_DECIMALS = 18;
     
-    uint256 public constant OWNER_SUPPLY = 1000000 * (10 ** uint256(TOKEN_DECIMALS));
-    uint256 public constant INITIAL_SUPPLY = 10000000 * (10 ** uint256(TOKEN_DECIMALS));
-    uint256 public constant CAP = 21000000 * (10 ** uint256(TOKEN_DECIMALS));
-    
-    uint256 public minerLicenses;
-    
+    uint256  constant OWNER_SUPPLY = 1000000 * (10 ** uint256(TOKEN_DECIMALS));
+    uint256  constant INITIAL_SUPPLY = 10000000 * (10 ** uint256(TOKEN_DECIMALS));
+    uint256  constant CAP = 21000000 * (10 ** uint256(TOKEN_DECIMALS));
+        
     mapping(address => uint) balances;
     mapping(address => mapping(address => uint)) allowed;
 
@@ -30,23 +28,8 @@ contract MichalPolakToken is ERC20, ERC20Detailed, ERC20Capped, ERC20Burnable, O
         ERC20Burnable()
         Ownable() {
        
-        addMinter(address(this));
         _mint(address(this), INITIAL_SUPPLY);
-        
-        addMinter(msg.sender);
         _mint(msg.sender, OWNER_SUPPLY);
-    }
-    
+    }    
 
-    function buyMinerLicense () public payable {
-      
-        addMinter(msg.sender);
-        minerLicenses++;
-    }
-    
-    function transferMinerLicence (address _to) public{
-       
-        renounceMinter(); 
-        addMinter(_to);
-    }
 }
