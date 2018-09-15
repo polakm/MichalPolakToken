4feda3754f9f84a15f3913d53bf7ad2ed03c8f91
michal <polak_m@o2.pl>
2018-09-15 14:44:59 +0200

    Refactor for burning tests

--- test/Burning.test.js
+++ test/Burning.test.js
@@ -10,7 +10,7 @@ require('chai')
   .use(require('chai-bignumber')(BigNumber))
   .should();
 
-contract('MichalPolakToken', function ([_, creator,account1,account2]) {
+contract('MichalPolakToken', function ([contract, creator, account1,account2]) {
   let token;
 
   const ZERO_ADDRESS = '0x0000000000000000000000000000000000000000';
@@ -22,33 +22,29 @@ contract('MichalPolakToken', function ([_, creator,account1,account2]) {
   });
 
 
-  //Po spaleniu tokenów przez podpalacza, jego saldo jest zmiejszony o ilość spalonych tokenów  
-  it('After burn tokens by burner, his balance is reduced by amount of burned tokens',async function () {
+  it('After burn tokens by contract owner, his balance is reduced by amount of burned tokens',async function () {
    
     //GIVEN
-    var burner = creator;
-    var balance = (await token.balanceOf(burner, { from: burner }));
     var amount = 1000;
+    var balance = (await token.balanceOf(owner, { from: owner }));
 
     //WHEN
-    await token.burn(amount, { from: burner });
+    await token.burn(amount, { from: owner });
     
     //THEN
-    (await token.balanceOf(burner, { from: burner })).should.be.bignumber.equal(balance.sub(amount));
+    (await token.balanceOf(owner, { from: owner })).should.be.bignumber.equal(balance.sub(amount));
 
   }); 
 
 
-  // Palący nie może spalić ujemnej ilości tokenów 
-  it('Burner can\'t burn a negative amount of tokens',async function () {
+  it('Contract owner can\'t burn a negative amount of tokens',async function () {
     
      //GIVEN
-     var burner = creator;
      var amount = -99;
      
      //WHEN
      try { 
-      await token.burn(amount, { from: burner });
+      await token.burn(amount, { from: owner });
       
       //THEN
     } catch (error) {
@@ -60,7 +56,6 @@ contract('MichalPolakToken', function ([_, creator,account1,account2]) {
   });
 
 
-  // Po spaleniu tokenów z konta sponsora przez palącego, saldo sponsora zmniejsza się o ilość spalonych żetonów 
   it('After burn tokens from sponsor account by burner, the sponsor balance is reduced by amount of burned tokens',async function () {
    
     //GIVEN
@@ -78,27 +73,26 @@ contract('MichalPolakToken', function ([_, creator,account1,account2]) {
 
   });
 
-    // Po spaleniu tokenów z konta sponsora przez palącego, saldo palącego pozostaje bez zmian
-    it('After burn tokens from sponsor account by burner, the burner account balance is not changed',async function () {
+
+  it('After burn tokens from sponsor account by burner, the burner account balance is not changed',async function () {
    
-      //GIVEN
-      var sponsor = creator;
-      var burner = account1;
-      var amount = 10000;
-      await token.transfer(burner, amount, { from: sponsor });
-      var balance = (await token.balanceOf(burner,{ from: burner }));
-      await token.approve(burner, amount, { from: sponsor });
+    //GIVEN
+    var sponsor = creator;
+    var burner = account1;
+    var amount = 10000;
+    await token.transfer(burner, amount, { from: sponsor });
+    var balance = (await token.balanceOf(burner,{ from: burner }));
+    await token.approve(burner, amount, { from: sponsor });
   
-      //WHEN
-      await token.burnFrom(sponsor, amount, { from: burner });
+    //WHEN
+    await token.burnFrom(sponsor, amount, { from: burner });
   
-      //THEN
-      (await token.balanceOf(burner, { from: burner })).should.be.bignumber.equal(balance);
+    //THEN
+    (await token.balanceOf(burner, { from: burner })).should.be.bignumber.equal(balance);
   
-    });
+  });
 
 
-  //Palący nie może spalić więcej tokenów z konta sponsora niż zatwierdzony przez sponsora przydział
   it('Burner can\'t burn the more tokens from sponsor account than approved by sponsor allowance',async function () {
    
     //GIVEN
@@ -120,7 +114,7 @@ contract('MichalPolakToken', function ([_, creator,account1,account2]) {
 
   });
 
-  //Palący nie można spalić ujemnej ilości tokenów z konta sponsora 
+
   it('Burner can\'t burn a negative amount of tokens from sponsor account',async function () {
    
     //GIVEN
