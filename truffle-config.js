ef203e5354fc655a3d9b106c82decc37552321c7
michal <polak_m@o2.pl>
2018-10-25 18:50:18 +0200

    Remove useless networks

--- truffle-config.js
+++ truffle-config.js
@@ -19,23 +19,11 @@ module.exports = {
     development: {
       host: 'localhost',
       port: 8545,
-      network_id: '*', // eslint-disable-line camelcase
+      network_id: '*', 
     },
     ropsten: {
       provider: ropstenProvider,
-      network_id: 3, // eslint-disable-line camelcase
-    },
-    coverage: {
-      host: 'localhost',
-      network_id: '*', // eslint-disable-line camelcase
-      port: 8555,
-      gas: 0xfffffffffff,
-      gasPrice: 0x01,
-    },
-    ganache: {
-      host: 'localhost',
-      port: 8545,
-      network_id: '*', // eslint-disable-line camelcase
+      network_id: 3, 
     },
   },
 };
